506d36399c3182a23692a2b5949a97e47259faf8
Fangjun Kuang <csukuangfj@gmail.com>
2023-03-08 21:39:32 +0800

    Support saving text with Unicode codepoint

--- textsearch/python/textsearch/datatypes.py
+++ textsearch/python/textsearch/datatypes.py
@@ -1,7 +1,8 @@
 import json
 from dataclasses import dataclass
+from itertools import chain
 from pathlib import Path
-from typing import Optional, List, Union
+from typing import List, Optional, Union
 
 import numpy as np
 
@@ -9,55 +10,72 @@ import numpy as np
 @dataclass
 class TextSource:
     """
-    Represents the full text from a UTF-8 encoded document.
-    We store the contents in memory as a numpy byte array,
-    together with the start positions of each Unicode symbol contained within.
+    Represents the full text of a utf-8 document.
+
+    The user can choose whether to save it in a utf-8 encoded np.uint8 array
+    or to save its Unicode codepoint into a np.int32 array.
     """
 
     # filename or ID
     name: str
 
-    # the text, probably as a sequence of bytes but could be utf-32 i.e. np.int32.
+    # It can be either a np.uint8 array or a np.int32 array.
+    # If it is a np.uint8 array, it contains the utf-8 encoded bytes;
+    # If it is a np.int32 array, it contains the Unicode codepoint of the text.
     # Length of the array may not be >= 2**32.
     binary_text: np.ndarray
 
-    # Only used if text is an np.int32: the mapping from
-    # UTF character position to byte position.
+    # Only used when binary_text.dtype is np.int32.
+    # It contains the mapping from utf-8 character position to byte position.
+    # That is pos[i] contains the byte position in binary_text for the i-th
+    # utf-8 character
     pos: Optional[np.ndarray] = None
 
     @property
     def text(self) -> str:
         """Return Python string representation of self.binary_text decoded as UTF-8."""
-        return self.binary_text.tobytes().decode("utf-8")
+        if self.binary_text.dtype == np.uint8:
+            return self.binary_text.tobytes().decode("utf-8")
+        else:
+            assert self.binary_text.dtype == np.int32, self.binary_text.dtype
+            return "".join([chr(i) for i in self.binary_text])
 
     @staticmethod
-    def from_file(path: Union[str, Path]) -> "TextSource":
-        binary_text = Path(path).read_bytes()
-        # try to decode it to trigger UnicodeDecodeError when it's not valid UTF-8
-        binary_text.decode("utf-8")
-        pos = _find_byte_offsets_for_utf8_symbols(binary_text)
-        return TextSource(
-            name=str(path),
-            binary_text=np.frombuffer(binary_text, dtype="S1"),
-            pos=pos,
-        )
+    def from_str(name: str, s: str, use_utf8: bool) -> "TextSource":
+        """Construct an instance of TextSource from a string.
 
-    @staticmethod
-    def from_str(name: str, s: str) -> "TextSource":
-        binary_text = s.encode("utf-8")
-        pos = _find_byte_offsets_for_utf8_symbols(binary_text)
-        return TextSource(
-            name=name,
-            binary_text=np.frombuffer(binary_text, dtype="S1"),
-            pos=pos,
-        )
+        Args:
+          name:
+            Name of the returned instance. It can be either a filename or an ID.
+          s:
+            It contains the text string.
+          use_utf8:
+            True to encode the text with utf-8.
+            False to save the Unicode codepoint of the text.
+        """
+        if use_utf8:
+            binary_text = s.encode("utf-8")
+            return TextSource(
+                name=name,
+                binary_text=np.frombuffer(binary_text, dtype=np.uint8),
+                pos=None,
+            )
+        else:
+            binary_text = np.fromiter((ord(i) for i in s), dtype=np.int32, count=len(s))
+            pos = _find_byte_offsets_for_utf8_symbols(binary_text)
+
+            return TextSource(
+                name=name,
+                binary_text=binary_text,
+                pos=pos,
+            )
 
 
-def _find_byte_offsets_for_utf8_symbols(binary_text: bytes) -> np.ndarray:
+def _find_byte_offsets_for_utf8_symbols(binary_text: np.ndarray) -> np.ndarray:
     """
     Args:
       binary_text:
-        A 1-D array containing utf-8 encoded characters.
+        A 1-D int32 array containing Unicode codepoint of characters.
     Returns:
       Return a np.uint32 1-D array. Its length equals to the number of
       characters contained in binary_text before encoding. ans[i]
@@ -67,16 +85,14 @@ def _find_byte_offsets_for_utf8_symbols(binary_text: bytes) -> np.ndarray:
 
     # Iterate over each byte in the binary text
     byte_index = 0
-    while byte_index < len(binary_text):
-        # Determine the length of the UTF-8 symbol starting at the current byte
-        byte = binary_text[byte_index]
-        if byte < 0x80:
+    for i in binary_text:
+        if i < 0x80:
             # Single-byte UTF-8 symbol
             symbol_length = 1
-        elif byte < 0xE0:
+        elif i < 0x800:
             # Two-byte UTF-8 symbol
             symbol_length = 2
-        elif byte < 0xF0:
+        elif i < 0x10000:
             # Three-byte UTF-8 symbol
             symbol_length = 3
         else:
@@ -107,9 +123,13 @@ class Transcript:
 
     # the text as a sequence of bytes or (more likely) int32 corresponding to UTF codepoints.
     # We will have to expand BPE tokens into bytes, presumably converting _ into space.
+    #
+    # When its dtype is np.uint8, it is encoded with utf-8
+    # When its dtype is np.int32, it contains the Unicode code point of characters
     binary_text: np.ndarray
 
     # Gives the time in seconds for each byte of the text.  (Should be in non-decreasing order).
+    # Its dtype is np.float32
     times: np.ndarray
 
     @property
@@ -118,7 +138,7 @@ class Transcript:
         return self.binary_text.tobytes().decode("utf-8")
 
     @staticmethod
-    def from_dict(name: str, d: dict) -> "Transcript":
+    def from_dict(name: str, d: dict, use_utf8: bool) -> "Transcript":
         """
         Args:
           name:
@@ -130,6 +150,9 @@ class Transcript:
 
               - d["begin_times]: List[float]. len(d["text"]) == d["begin_times"].
                 d["begin_times"][i] is the begin time for d["text"][i]
+          use_utf8:
+            True to use utf-8 to encode the text.
+            False to save the Unicode code point of the text.
         """
         assert "text" in d, list(d.keys())
         assert "begin_times" in d, list(d.keys())
@@ -140,26 +163,48 @@ class Transcript:
             d["begin_times"],
         )
 
-        bytes_list = []
-        times_list = []
-        for text, begin_time in zip(d["text"], d["begin_times"]):
-            b = text.encode("utf-8")
+        if use_utf8:
+            byte_list = []
+            time_list = []
+            for text, begin_time in zip(d["text"], d["begin_times"]):
+                b = text.encode("utf-8")
 
-            if times_list:
-                # Check that begin_time is non-decreasing.
-                #
-                # < here requires that it is strictly increasing.
-                assert times_list[-1] < begin_time, (times_list[-1], begin_time)
+                if time_list:
+                    # Check that begin_time is non-decreasing.
+                    #
+                    # < here requires that it is strictly increasing.
+                    assert time_list[-1] < begin_time, (time_list[-1], begin_time)
 
-            # bytes belonging to the same text have the same begin time
-            times_list += [begin_time] * len(b)
-            bytes_list.append(b)
+                # bytes belonging to the same text have the same begin time
+                time_list += [begin_time] * len(b)
+                byte_list.append(b)
 
-        return Transcript(
-            name=name,
-            binary_text=np.frombuffer(b"".join(bytes_list), dtype="S1"),
-            times=np.asarray(times_list, dtype=np.float32),
-        )
+            return Transcript(
+                name=name,
+                binary_text=np.frombuffer(b"".join(byte_list), dtype=np.uint8),
+                times=np.asarray(time_list, dtype=np.float32),
+            )
+        else:
+            codepoint_list = []
+            time_list = []
+            for text, begin_time in zip(d["text"], d["begin_times"]):
+                codepoint_list.append(ord(i) for i in text)
+
+                if time_list:
+                    # Check that begin_time is non-decreasing.
+                    #
+                    # < here requires that it is strictly increasing.
+                    assert time_list[-1] < begin_time, (time_list[-1], begin_time)
+
+                # bytes belonging to the same text have the same begin time
+                # Each character occupies 4 bytes, so it is multiplied by 4
+                time_list += [begin_time] * (len(text) * 4)
+
+            return Transcript(
+                name=name,
+                binary_text=np.fromiter(chain(*codepoint_list), dtype=np.int32),
+                times=np.asarray(time_list, dtype=np.float32),
+            )
 
 
 # we'll have a global list of text sources during the program lifetime, and indexes into this list
