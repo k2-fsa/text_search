46e604f1d865d86d427af27b1264a3a9b824d249
Fangjun Kuang <csukuangfj@gmail.com>
2023-03-12 18:26:41 +0800

    Support saving text with its Unicode code points. (#16)
    * Support saving text with Unicode codepoint
    
    * small fixes
    
    * Fix CI

--- textsearch/python/tests/test_text_source.py
+++ textsearch/python/tests/test_text_source.py
@@ -1,46 +1,60 @@
 #!/usr/bin/env python3
 
 import unittest
-from tempfile import NamedTemporaryFile
 
 import numpy as np
+
 from textsearch import TextSource
 
 
 class TestTextSource(unittest.TestCase):
-    def test(self):
-        with NamedTemporaryFile(suffix=".txt", encoding="utf8", mode="w+") as f:
-            s = "zażółć gęślą jaźń\n你好Hallo"
-            f.write(s)
-            f.flush()
-            f.seek(0)
+    def test_use_utf8(self):
+        name = "test"
+        s = "zażółć gęślą jaźń\n你好Hallo"
+        source = TextSource.from_str(name=name, s=s, use_utf8=True)
 
-            source = TextSource.from_file(f.name)
+        assert source.name == name, (source.name, name)
 
-            assert isinstance(source.name, str)
-            assert source.name == f.name, (source.name, f.name)
+        assert isinstance(source.binary_text, np.ndarray)
+        assert source.binary_text.dtype == np.uint8, source.binary_text.dtype
 
-            assert isinstance(source.binary_text, np.ndarray)
-            assert np.issubdtype(source.binary_text.dtype, "S1")
-            s_bytes = s.encode("utf-8")
-            assert source.binary_text.tobytes() == s_bytes
+        expected_binary_text = s.encode("utf-8")
+        assert source.binary_text.tobytes() == expected_binary_text
 
-            assert isinstance(source.text, str)
-            assert source.text == s
+        assert isinstance(source.text, str), type(source.text)
+        assert source.text == s, (source.text, s)
 
-            assert isinstance(source.pos, np.ndarray)
-            assert np.issubdtype(source.pos.dtype, np.uint32), source.pos.dtype
-            # fmt: off
-            # A Polish character occupies 2 bytes in utf-8 while
-            # a Chinese character occupies 3 bytes
-            np.testing.assert_equal(
-                source.pos,
-                np.array(
-                    [0, 1, 2, 4, 6, 8, 10, 11, 12, 14, 16, 17, 19, 20, 21, 22, 24,
-                     26, 27, 30, 33, 34, 35, 36, 37]
-                ),
-            )
-            # fmt: on
+        assert source.pos is None, source.pos
+
+    def test_without_using_utf8(self):
+        name = "test"
+        s = "zażółć gęślą jaźń\n你好Hallo"
+        source = TextSource.from_str(name=name, s=s, use_utf8=False)
+
+        assert source.name == name, (source.name, name)
+
+        assert isinstance(source.binary_text, np.ndarray)
+        assert source.binary_text.dtype == np.int32, source.binary_text.dtype
+
+        expected_binary_text = np.fromiter((ord(i) for i in s), dtype=np.int32)
+        np.testing.assert_equal(source.binary_text, expected_binary_text)
+
+        assert isinstance(source.text, str), type(source.text)
+        assert source.text == s, (source.text, s)
+
+        assert isinstance(source.pos, np.ndarray), type(source.pos)
+        assert source.pos.dtype == np.uint32, source.pos.dtype
+        # fmt: off
+        # A Polish character occupies 2 bytes in utf-8 while
+        # a Chinese character occupies 3 bytes
+        np.testing.assert_equal(
+            source.pos,
+            np.array(
+                [0, 1, 2, 4, 6, 8, 10, 11, 12, 14, 16, 17, 19, 20, 21, 22, 24,
+                 26, 27, 30, 33, 34, 35, 36, 37], dtype=np.uint32
+            ),
+        )
+        # fmt: on
 
 
 if __name__ == "__main__":
