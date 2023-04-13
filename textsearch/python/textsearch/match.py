d5d920fd67e06de09f50585b7e45ab220d73510d
pkufool <wkang.pku@gmail.com>
2023-04-13 12:10:18 +0800

    Add tests in python level

--- textsearch/python/textsearch/match.py
+++ textsearch/python/textsearch/match.py
@@ -24,9 +24,33 @@ from _fasttextsearch import (
 def get_longest_increasing_pairs(
     seq1: np.ndarray, seq2: np.ndarray
 ) -> List[Tuple[int, int]]:
+    """
+    Get the longest increasing pairs for given sequences.
+    See https://github.com/danpovey/text_search/issues/21 for more details.
+
+    Suppose seq1 is [i1, i2, i3... iN] and seq2 is [j1, j2, j3... jN], this
+    function returns the  longest increasing pairs: (i1, j1), (i2, j2), ... (iN, jN)
+    such that i1 <= i2 <= ... <= iN, and j1 <= j2 <= ... <= jN.
+
+    Args:
+      seq1:
+        The first sequence.
+      seq2:
+        The second sequence.
+
+    >>> import numpy as np
+    >>> from textsearch import get_longest_increasing_pairs
+    >>> seq1 = np.array([0, 1, 1, 2, 2, 3, 4, 5, 6], dtype=np.int32)
+    >>> seq2 = np.array([9, 7, 8, 9, 6, 7, 10, 12, 8], dtype=np.int64)
+    >>> get_longest_increasing_pairs(seq1=seq1, seq2=seq2)
+    [(5, 12), (4, 10), (2, 9), (1, 8), (1, 7)]
+
+    """
     assert seq1.ndim == 1, seq1.ndim
     assert seq2.ndim == 1, seq2.ndim
+    assert seq1.size == seq2.size, (seq1.size, seq2.size)
 
+    # The sequences are required to be contiguous int32 array in C++.
     seq1_int32 = np.ascontiguousarray(seq1, dtype=np.int32)
     seq2_int32 = np.ascontiguousarray(seq2, dtype=np.int32)
 
