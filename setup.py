32a34d6ae406666fdf112c3c7548a7196ffb1b74
Wei Kang <wkang.pku@gmail.com>
2023-07-25 11:29:30 +0800

    Wheels (#47)
    * Fix build wheel
    
    * copy so
    
    * Minor fixes
    
    * minor fixes
    
    * Minor fixes
    
    * Fixes
    
    * Upgrade to version 0.7

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 
 import glob
 import os
+import platform
 import re
 import shutil
 import sys
@@ -12,6 +13,10 @@ import setuptools
 from setuptools.command.build_ext import build_ext
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+def is_windows():
+    return platform.system() == "Windows"
 
 
 def cmake_extension(name, *args, **kwargs) -> setuptools.Extension:
@@ -37,7 +42,8 @@ class BuildExtension(build_ext):
         system_make_args = os.environ.get("MAKEFLAGS", "")
 
         if cmake_args == "":
-            cmake_args = "-DCMAKE_BUILD_TYPE=Release -DTS_BUILD_TESTS=OFF"
+            cmake_args = "-DCMAKE_BUILD_TYPE=Release -DTS_ENABLE_TESTS=OFF"
+            cmake_args += f" -DCMAKE_INSTALL_PREFIX={self.build_lib} "
 
         if make_args == "" and system_make_args == "":
             make_args = " -j "
@@ -46,24 +52,44 @@ class BuildExtension(build_ext):
             print(f"Setting PYTHON_EXECUTABLE to {sys.executable}")
             cmake_args += f" -DPYTHON_EXECUTABLE={sys.executable}"
 
-        build_cmd = f"""
-            cd {self.build_temp}
 
-            cmake {cmake_args} {ts_dir}
-
-            make {make_args} _textsearch
-        """
-        print(f"build command is:\n{build_cmd}")
-
-        ret = os.system(build_cmd)
-        if ret != 0:
-            raise Exception(
-                "\nBuild text_search failed. Please check the error "
-                "message.\n"
-                "You can ask for help by creating an issue on GitHub.\n"
-                "\nClick:\n"
-                "\thttps://github.com/k2-fsa/text_search/issues/new\n"  # noqa
+        if is_windows():
+            build_cmd = f"""
+         cmake {cmake_args} -B {self.build_temp} -S {ts_dir}
+         cmake --build {self.build_temp} --target install --config Release -- -m
+            """
+            print(f"build command is:\n{build_cmd}")
+            ret = os.system(
+                f"cmake {cmake_args} -B {self.build_temp} -S {ts_dir}"
             )
+            if ret != 0:
+                raise Exception("Failed to configure fasttextsearch")
+
+            ret = os.system(
+                f"cmake --build {self.build_temp} --target install --config Release -- -m"  # noqa
+            )
+            if ret != 0:
+                raise Exception("Failed to build and install fasttextsearch")
+        else:
+            build_cmd = f"""
+                cd {self.build_temp}
+
+                cmake {cmake_args} {ts_dir}
+
+                make {make_args} install/strip
+            """
+            print(f"build command is:\n{build_cmd}")
+
+            ret = os.system(build_cmd)
+            if ret != 0:
+                raise Exception(
+                    "\nBuild text_search failed. Please check the error "
+                    "message.\n"
+                    "You can ask for help by creating an issue on GitHub.\n"
+                    "\nClick:\n"
+                    "\thttps://github.com/k2-fsa/text_search/issues/new\n"  # noqa
+                )
+
         lib_so = glob.glob(f"{build_dir}/lib/*.so*")
         for so in lib_so:
             print(f"Copying {so} to {self.build_lib}/")
@@ -85,14 +111,25 @@ def get_package_version():
     return latest_version
 
 
-with open("textsearch/python/textsearch/__init__.py", "a") as f:
-    f.write(f"__version__ = '{get_package_version()}'\n")
-
 setuptools.setup(
     package_dir={
         "textsearch": "textsearch/python/textsearch",
     },
     packages=["textsearch"],
-    ext_modules=[cmake_extension("_text_search")],
+    ext_modules=[cmake_extension("_textsearch")],
     cmdclass={"build_ext": BuildExtension},
 )
+
+with open("textsearch/python/textsearch/__init__.py", "a") as f:
+    f.write(f"__version__ = '{get_package_version()}'\n")
+
+with open("textsearch/python/textsearch/__init__.py", "r") as f:
+    lines = f.readlines()
+
+with open("textsearch/python/textsearch/__init__.py", "w") as f:
+    for line in lines:
+        if "__version__" in line:
+            f.write(line)
+            break
+        f.write(line)
+
